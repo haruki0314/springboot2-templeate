f2aecfdd7a67c5b076eb9f4d4e2a42e3af248dc4
haru <haruki1472@icloud.com>
2022-08-08 21:55:28 +0900

    initital Commit

---
+++ src/test/java/com/example/hoge/Springboot2TempleateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hoge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2TempleateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
